ea860bd4e22e3e0b67e1c26d292d792a1be3a6c0
Admin <ndh1601204@gmail.com>
2024-07-16 19:57:48 +0700

    Commit message here

---
+++ Todo.js
@@ -0,0 +1,10 @@
+// TodoItem.js
+export default class TodoItem {
+    constructor(id, title, content, isCompleted) {
+      this.id = id;
+      this.title = title;
+      this.content = content;
+      this.isCompleted = isCompleted;
+    }
+  }
+  
