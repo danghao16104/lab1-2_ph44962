1624cb27e7888f7575366d3366afe628ed05f993
Admin <ndh1601204@gmail.com>
2024-07-16 19:28:14 +0700

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { Button, ScrollView, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import Block from './Components/Block'
+import Banner from './Components/Banner'
+import Btn from './Components/Btn'
+import TIP from './Components/TextIP'
+import {Theme,useTheme} from './Components/Theme'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <Theme>
+     <ScrollView>
+     <Body/>
+     </ScrollView>
+    </Theme>
+  )
+}
+const Body=()=>{
+  const [ht, setHt] = useState('');
+  const [tuoi, setTuoi] = useState('');
+  const [idkhoa, setIdkhoa] = useState('');
+  const {theme,toggleTheme}=useTheme();
+  return(
+    <View style={{justifyContent:'center',alignItems:'center' ,flex:1, padding:10, backgroundColor:theme==='light'?'#EEEEEE':'#000000'}}>
+      <Banner/>
+      <Block title='Thông tin cá nhân'>
+        <TIP hint='Nhập họ và tên' oct={setHt} />
+        <TIP hint='Nhập tuổi' oct={setTuoi} />
+      </Block>
+      <Block title='Thông tin khóa học'>
+        <TIP hint='Nhập mã khóa học' oct={setHt} />
+        <TIP hint='Nhập tên khóa học' oct={setTuoi} />
+      </Block>
+      <Block title='Thông tin liên hệ'>
+       <Text style={{fontSize:18}}>Vui lòng liên hệ số điện thoại: 0974832596</Text>
+      </Block>
+      <Btn nut='Đổi giao diện' OP={toggleTheme} />
     </View>
-  );
+  )
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default App
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+const styles = StyleSheet.create({})
